330dc37e9fd6e950b2e50a154689cf79d267f9b0
KlausStille <mail@klausstille.com>
2023-12-09 19:21:08 +0100

    /layout tweaks

--- components/FilterProjects/FilterProjects.tsx
+++ components/FilterProjects/FilterProjects.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useColorThemeStore } from "@/stores/colorTheme";
 import FilterEntryComponent from "./FilterEntryComponent";
 import MenuIcon from "./MenuIcon";
@@ -17,17 +17,43 @@ export default function FilterProjects({
     const darkTheme = useColorThemeStore(
         (state: unknown) => (state as any).darkTheme
     );
+    const menuRef = useRef<HTMLDivElement>(null);
+
+    const handleBackgroundClick = (event: MouseEvent) => {
+        const clickedElement = event.target as HTMLElement;
+
+        if (
+            menuRef.current &&
+            !menuRef.current.contains(event.target as Node) &&
+            !clickedElement.classList.contains("menu-button")
+        ) {
+            setIsOpen(false);
+            console.log("click outside");
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener("click", handleBackgroundClick);
+        return () => {
+            document.removeEventListener("click", handleBackgroundClick);
+        };
+    }, []);
+
     return (
         <>
+            <h1
+                className={`menu-button fixed bottom-0 right-0 py-2 px-2 z-40 text-m cursor-pointer`}
+                onClick={handleClick}
+            >
+                MENU
+            </h1>
             <section
                 className={`${
-                    isOpen ? "opacity-100" : "opacity-0"
-                } transition-opacity duration-[.2s] ease-in-out fixed w-1/5 bottom-0 pr-2 right-0 py-2 px-2 ${
+                    isOpen ? "menu-open" : "menu-closed"
+                } transition-opacity duration-[.2s] ease-in-out fixed w-1/5 bottom-2 pr-2 right-2 py-2 px-2 ${
                     darkTheme ? "bg-real-black" : "bg-white"
-                } h-full overflow-scroll z-40`}
-                style={{
-                    transform: isOpen ? "translateX(0)" : "translateX(100%)",
-                }}
+                } h-[calc(100vh-1rem)] overflow-scroll z-40 rounded-md`}
+                ref={menuRef}
             >
                 <MenuIcon
                     isOpen={isOpen}
@@ -40,12 +66,6 @@ export default function FilterProjects({
                     setFilterIsActive={setFilterIsActive}
                 />
             </section>
-            <h1
-                className={`fixed bottom-0 right-0 py-2 px-2 z-40 text-m cursor-pointer`}
-                onClick={handleClick}
-            >
-                {isOpen ? "CLOSE" : "MENU"}
-            </h1>
         </>
     );
 }
