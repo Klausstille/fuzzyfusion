454063a3616cc1197de5c6e344a539a4796ee39a
KlausStille <mail@klausstille.com>
2023-12-10 19:54:04 +0100

    /fix dotenv adn next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
+require("dotenv").config();
+
+module.exports = {
     images: {
-        domains: ["images.ctfassets.net"],
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "images.ctfassets.net",
+            },
+        ],
+    },
+    env: {
+        CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
+        CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
     },
 };
-
-module.exports = nextConfig;
